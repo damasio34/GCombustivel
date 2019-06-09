e819e9dc234c507784fdecc73116d234a318950f
Darlan Damasio <damasio34@gmail.com>
2019-06-09 14:06:56 -0300

    Criação do serviço de leitura de arquivos

---
+++ Aplicacao/ArquivoService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Aplicacao
+{
+    public class ArquivoService
+    {
+        private readonly string _path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+        public Queue<string> LerArquivoDeEntrada(string nomeDoArquivo)
+        {
+            var linhas = File.ReadAllLines($"{_path}/Inputs/{nomeDoArquivo}");
+            var queue = new Queue<string>(linhas);
+
+            return queue;
+        }
+        public void EscreverArquivoDeSaida(Queue<string> linhas, string nomeDoArquivo)
+        {
+            File.WriteAllLines($"{_path}/Inputs/{nomeDoArquivo}", linhas);
+        }
+    }
+}
---
+++ GCombustivel.Teste/ArquivoTest.cs
@@ -0,0 +1,20 @@
+﻿using Aplicacao;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Damasio34.GCombustivel.Teste
+{
+    [TestClass]
+    public class ArquivoTest
+    {
+        [TestMethod]
+        public void Ler_arquivo_de_entrada()
+        {
+            var arquivoService = new ArquivoService();
+            var queue = arquivoService.LerArquivoDeEntrada("entrada_veiculos_teste.txt");
+
+            Assert.IsNotNull(queue);
+            Assert.AreEqual(queue.Count(), 5);
+        }
+    }
+}
